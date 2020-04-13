af03c9be0e5dcb6443e11aa647a32142ebe02532
Dario Gagulic <dario.gagulic@gmail.com>
2020-04-13 11:10:07 +0200

    data.sql added 13 cards, deleted cardController

--- src/main/java/ch/uzh/ifi/seal/soprafs20/service/CardService.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs20/service/CardService.java
@@ -19,51 +19,44 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.time.LocalDate;
 import java.util.*;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 
 /**
  * User Service
  * This class is the "worker" and responsible for all functionality related to the user
  * (e.g., it creates, modifies, deletes, finds). The result will be passed back to the caller.
  */
-@Service
-@Transactional
+
 public class CardService {
 
-    private final Logger log = LoggerFactory.getLogger(GameService.class);
+    public static void main(String[] args){
+            try {
+                Reader cardsEN = new File(System.getProperty("user.dir")+"/src/main/java/ch/uzh/ifi/seal/soprafs20/cards-EN.txt");
+                File dataSQL = new File(System.getProperty("user.dir")+"/src/main/data.sql");
+                BufferedReader reader = new BufferedReader(cardsEN);
+                FileWriter fw = new FileWriter(dataSQL);
+                try {
 
-    private final CardRepository cardRepository;
+                    String word = reader.readLine();
+                    while (word!=null){
 
-    @Autowired
-    public CardService(@Qualifier("cardRepository") CardRepository cardRepository) {
+                    }
 
-        this.cardRepository = cardRepository;
-
-    }
-
-    public void fillCardTable(){
-        try {
-            FileReader fr = new FileReader(System.getProperty("user.dir")+"/src/main/java/ch/uzh/ifi/seal/soprafs20/cards-EN.txt");   //reads the file
-            BufferedReader br = new BufferedReader(fr);  //creates a buffering character input stream
-            String word1;
-            while((word1 = br.readLine())!=null) {
-                Card card = new Card();
-                card.setWord1(word1);
-                card.setWord2(br.readLine());
-                card.setWord3(br.readLine());
-                card.setWord4(br.readLine());
-                card.setWord5(br.readLine());
-                br.readLine();
-                cardRepository.save(card);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }finally{
+                    //close resources
+                    try {
+                        fw.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                fr.close();    //closes the stream and release the resources
+            } catch (IOException e){
+                e.printStackTrace();
             }
-            fr.close();    //closes the stream and release the resources
-        } catch (IOException e){
-            e.printStackTrace();
-        }
-    }
+
 }
