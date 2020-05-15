b706d9c7758fe3e5e320d873d76c43872d271143
lzumta <lynn.zumtaugwald@uzh.ch>
2020-05-15 09:23:00 +0200

    moved gameServiceIntegrationTest

--- src/test/java/ch/uzh/ifi/seal/soprafs20/service/GameServiceIntegrationTest.java
+++ src/test/java/ch/uzh/ifi/seal/soprafs20/service/GameServiceIntegrationTest.java
@@ -1,5 +1,5 @@
-/*
 package ch.uzh.ifi.seal.soprafs20.service;
+
 import ch.uzh.ifi.seal.soprafs20.constant.GameStatus;
 import ch.uzh.ifi.seal.soprafs20.constant.UserStatus;
 import ch.uzh.ifi.seal.soprafs20.entity.*;
@@ -12,13 +12,20 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.web.server.ResponseStatusException;
+
 import static org.junit.jupiter.api.Assertions.*;
-*/
-/*
+
+/**
+ * Test class for the UserResource REST resource.
+ * @see UserService
+ */
 @WebAppConfiguration
 @SpringBootTest
-class GameServiceIntegrationTest {
+public class GameServiceIntegrationTest {
 
     @Qualifier("gameRepository")
     @Autowired
@@ -35,29 +42,33 @@ class GameServiceIntegrationTest {
     @Autowired
     private GameService gameService;
 
+    @Autowired
+    private UserService userService;
+
     @BeforeEach
     public void setup() {
+        playerRepository.deleteAll();
         gameRepository.deleteAll();
         userRepository.deleteAll();
-        playerRepository.deleteAll();
     }
 
     @Test
     public void createGame_validInputs_success() {
         // given -> gameName not used yet
-        assertNull(gameRepository.findByGameName(Mockito.anyString()));
+
         User user = new User();
         user.setUsername("testUser");
         user.setPassword("testPassword");
         user.setStatus(UserStatus.ONLINE);
-        userRepository.save(user);
+        User user1 = userRepository.save(user);
         userRepository.flush();
 
-
+        //assertNull(gameRepository.findByGameName(Mockito.anyString()));
+        //Mockito.when(userRepository.findByUserId(Mockito.anyLong())).thenReturn(user);
 
         Game testGame = new Game();
         testGame.setGameName("testGameName");
-        testGame.setAdminPlayerId(1L);
+        testGame.setAdminPlayerId(user1.getUserId());
         testGame.setHasBot(true);
 
         Game createdGame = gameService.createNewGame(testGame);
@@ -74,11 +85,8 @@ class GameServiceIntegrationTest {
         assertEquals(2, createdGame.getNumberOfPlayers());
         assertTrue(createdGame.getPlayers().get(0) instanceof PhysicalPlayer);
         assertTrue(createdGame.getPlayers().get(1) instanceof MaliciousBot
-                    || createdGame.getPlayers().get(1) instanceof FriendlyBot);
+                || createdGame.getPlayers().get(1) instanceof FriendlyBot);
 
 
     }
-
-
-
-}*/
+}
